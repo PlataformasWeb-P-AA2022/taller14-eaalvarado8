c697c596b6980020d20c6a96a9b2d5eb1c8b21ea
eaalvarado8 <eric_alvarado2202@hotmail.com>
2022-07-19 17:47:52 -0500

    subida

--- app-django/ejemplo3/proyectoUno/proyectoUno/settings.py
+++ app-django/ejemplo3/proyectoUno/proyectoUno/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # agregar apps para Auth - paso 1
-    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
